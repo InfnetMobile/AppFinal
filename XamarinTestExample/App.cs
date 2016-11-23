c4bdd79d8d2671b81229670ecd1ba7353e4c0e46
ferna <ferna@DESKTOP-0PTTGUR>
2016-11-23 19:24:38 -0200

    Add project files.

---
+++ XamarinTestExample/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using XamarinTestExample.Views;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace XamarinTestExample
+{
+    public class App : PrismApplication
+    {
+        protected override void OnInitialized()
+        {
+            NavigationService.NavigateAsync("HomePage");
+        }
+
+        protected override void RegisterTypes()
+        {
+            
+
+            Container.RegisterTypeForNavigation<HomePage>();
+            Container.RegisterTypeForNavigation<UserInfoConfirmationPage>();
+            Container.RegisterInstance(Acr.UserDialogs.UserDialogs.Instance);
+        }
+    }
+}
