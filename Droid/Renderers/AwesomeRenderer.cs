527d4e4e0c942208677e2d74fb4ecb6f32664af4
Beatriz Macedo <beamb@br.ibm.com>
2016-11-30 23:07:13 -0200

    Fixed font awesome font by adding Awesome Renderer (from AppCasaCorretor) and font to android project

---
+++ Droid/Renderers/AwesomeRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.Graphics;
+using Android.Widget;
+using AppFinalMITInfnet.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(AwesomeRenderer))]
+namespace AppFinalMITInfnet.Droid
+{
+	public class AwesomeRenderer : LabelRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+
+			var label = (TextView)Control;
+
+			var text = label.Text;
+
+			if (string.IsNullOrEmpty(text) || text.Length > 1 || text[0] < 0xf000)
+			{
+				return;
+			}
+
+			var font = Typeface.CreateFromAsset(Context.ApplicationContext.Assets, "fontawesome-webfont.ttf");
+			label.Typeface = font;
+		}
+	}
+}
